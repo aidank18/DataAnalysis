edcdef6f665c94a270aa3bedc09b3e3e686363c9
aidank18 <65195050+aidank18@users.noreply.github.com>
2026-07-31 14:39:40 -0400

    Create cleanData.py

---
+++ cleanData.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("ahmedmohamed2003/cafe-sales-dirty-data-for-cleaning-training")
+
+print("Path to dataset files:", path)
