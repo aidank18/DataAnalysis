ccc04b90a4fbdf424d7ac9666fa2528a633cc45e
aidank18 <aidan.knerr@gmail.com>
2026-08-03 17:23:17 -0400

    Added Most Features

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # DataAnalysis
-A repo containing personal projects relating to data analysis, cleaning, and visualization
+Welcome to my project on data analysis and cleaning!
+
+
+# Problem Description
+In this project I used fictional data detailing the sales of a cafe in 2023. I wanted to find out 
+which products the cafe was making the most profit off of and how their sales changed as the year progressed.
+This would allow me to advise the cafe on which products they should focus on to improve profits and help me identify
+any slumps in sales.
+
+# General Approach
+My general approach for this project was to first clean the data using Python and its built in csv library
+before exporting the cleaned data into Tableau where I could create clean and interactive visuals (linked below). These 
+visuals would be key in helping me explain my findings to theoretical stakeholders.
+
+# Problems Encountered
+In general, the biggest problems came from the data themselves. The data came in a csv where each line corrosponded to
+one transaction. Each transaction was supposed to have basic data as follows: 
+
+    Transaction ID, Item, Quantity, Price Per Unit, Total Spent, Payment Method, Location, Transaction Date
+
+Unfortunately there were many lines containing a mix of missing values, error messages, unknown values, and NaNs. 
+As I didnt know which lines contained errors nor what the error values would be I had to build in checks to each value 
+I looked at. Using Python I looped through each transaction and performed a series of if else checks on each value in 
+the transaction to make sure it was valid. If the value was not valid I would either use the rest of the information in
+the transaction to fill in the value, or if it couldnt be known I would set it to a standard UNKOWN. For example, if the
+Total Spent was missing but I had the Price Per Unit and the Quantity then I could fill in Total Spent by taking
+Price Per Unit * Quantity. This allowed me to fill in a serious amount of lost data, thus improving my analysis.
+
+# Outcomes
+Since this was fictional data the actual conclusions were a little silly but I was able to create two useful visualizations
+that you can explore on Tableau. The first was a bar graph showing gross profit by item type sold over the whole year. 
+This allowed me to easily see that salads were the cafes most profitable product sold. By including fiters I can also see 
+sales month by month and see that salads were not just the most profitable over the whole year, but each month they were the 
+best sellers. The only month its close is September where smoothies almost become more profitable. The second graph I have 
+is a line graph showing the quantity of item sold split by month. Its not a very impressive graph becasue the data are randomly 
+generated but it allows me to see that in September while salads stayed most profitable the cafe actually sold a higher quantity 
+of smoothies, just more cheeply. 
+
+By using Tableau and setting it up so the two graphs can be filters for each other the data becomes highly interactable. 
+For example I can click on salad in the first graph and see the quantity sold graph change to reflect only the quantity 
+of salads. In the other direction I can click on a month on the second graph and watch the first change to represent only 
+gross profit for that month. I also have a seperate filter that allows me to remove certain months of data from both graphs. 
+This is largely useful just to simplify the quantity graph.
+
+
+# Visualization
+After cleaning the data I uploaded it to Tableau to create some example visualizations, link below.
+https://public.tableau.com/app/profile/aidan.knerr/viz/FictionalCafeSalesOver2023/Dashboard1
+Or you can search on tableau for "Fictional Cafe Sales Over 2023" by Aidan Knerr.
--- cleanData.py
+++ cleanData.py
@@ -1,6 +1,145 @@
-import kagglehub
+# Import Pythons built in csv lib
+import csv
 
-# Download latest version
-path = kagglehub.dataset_download("ahmedmohamed2003/cafe-sales-dirty-data-for-cleaning-training")
 
-print("Path to dataset files:", path)
+# In a normal project the file path would be a dynamic link passed in at runtime ideally from some sort of config file
+# Since this is just a personal project I will keep this value static. If you want to run this yourself just edit
+# The git_repo_path to point to wherever the git repo got cloned.
+# There is also some value in using the Python Path library to standardize these paths 
+# So they could work with any operating system as opposed to now where due to the \\ notation they only work on windows
+git_repo_path = "C:\\Users\\aidan\\Documents\\GitHub\\DataAnalysis\\"
+
+# These are the paths within the repo, these will always be static
+input_path = "archive_unzip\\dirty_cafe_sales.csv"
+output_path = "output\\clean_cafe_sales.csv"
+
+# Output full path for logging purposes
+print(git_repo_path + input_path)
+
+# Here we will define a short function to help us later. This will let us quickly test if a value is a number or not
+def is_any_num(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+
+
+# Here we create a dictionary of our valid items sold with their prices which will come in handy for checking
+# All items are uppercase as we will be standardizing capitalization to all uppercase
+valid_items = {"COFFEE" : 2, "TEA" : 1.5, "SANDWICH" : 4, "SALAD" : 5, "CAKE" : 3, "COOKIE" : 1, "SMOOTHIE" : 4, "JUICE" : 3}
+# We can also define some other valid options for the other fields of data
+valid_payments = {"CASH", "CREDIT CARD", "DIGITAL WALLET"}
+valid_places = {"IN-STORE", "TAKEAWAY"}
+
+# Our data comes in the form of a csv where each line is a different transaction record
+# Here we will open the file and begin looping through each line
+with open(git_repo_path + input_path) as file:
+
+    # This reads in the data so we can begin looping
+    reader = csv.DictReader(file)
+
+    # We will be writing the cleaned data to a new csv which we open here
+    with open(git_repo_path + output_path, 'w', newline = '') as out_file:
+        #opens the output file we will be writing data to
+        writer = csv.DictWriter(out_file, reader.fieldnames)
+        # Writes the header with the same columns as the unclean data
+        writer.writeheader()
+
+        # Here we actually begin looping
+        for response in reader:
+
+            # In each loop we will do a seperate check for each field that is supposed to be here, defined as:
+            # Transaction ID, Item, Quantity, Price Per Unit, Total Spent, Payment Method, Location, Transaction Date
+
+
+
+            # Theres not a lot of checking we can do for the ID but we can check if it exists, is its proper length and 
+            # That the first four characters are TXN_
+            id = response["Transaction ID"]
+            if not id:
+                id = 'UNKNOWN'
+            elif len(id) != 11:
+                id = 'UNKNOWN'
+            elif id[0:4] != "TXN_":
+                id = 'UNKNOWN'
+
+
+            # For the item we can check if its part of the valid_items dict we define above
+            # We also will standardize each item to be all uppercase
+            item = response["Item"].upper()
+            if item not in valid_items:
+                item = "UNKNOWN"
+
+
+            # For the quantity we can check if its a whole number with the Python isdigit function
+            quantity = response["Quantity"]
+            if not quantity.isdigit():
+                quantity = "UNKNOWN"
+
+            # For the price_per_unit we can use the above defined function to check it is any number
+            price_per_unit = response["Price Per Unit"]
+            if not is_any_num(price_per_unit):
+                # If price_per_unit is not a number but we do know what item it was then we can fix the error
+                if item != "UNKNOWN":
+                    price_per_unit = valid_items[item]
+                else:
+                    price_per_unit = "UNKNOWN"
+
+    
+            # Fir titak spent we will check its any number
+            total_spent = response["Total Spent"]
+            # if the total spent is not a valid value and we know the item and quantity then we can fix it
+            # else we set it to unknown
+            if not is_any_num(total_spent):
+                if price_per_unit != "UNKNOWN" and quantity != "UNKNOWN":
+                    total_spent = str(float(price_per_unit) * float(quantity))
+                else:
+                    total_spent = "UNKNOWN"
+
+            # Now that weve cleaned quantity, price per unit, and total spent we can go back
+            # and if we know two of the three we can calculate and fill in the third
+            if quantity == "UNKNOWN" and price_per_unit != "UNKNOWN" and total_spent != "UNKNOWN":
+                quantity = str(float(total_spent) / float(price_per_unit))
+            if quantity != "UNKNOWN" and price_per_unit == "UNKNOWN" and total_spent != "UNKNOWN":
+                price_per_unit = str(float(total_spent) / float(quantity))
+            # We already checked the third case when we cleaned total_spent so we can skip it here
+
+            # Here we check payment_methods against the above defined dict of valid_payments
+            payment_method = response["Payment Method"].upper()
+            if payment_method not in valid_payments:
+                payment_method = "UNKNOWN"
+
+
+            # Here we check location against the above defined dict of valid_places
+            location = response["Location"].upper()
+            if location not in valid_places:
+                location = "UNKNOWN"
+
+            transaction_date = response["Transaction Date"]    
+            if transaction_date != "":
+                if transaction_date.upper() == "ERROR" or transaction_date.upper() == "UNKNOWN":
+                    transaction_date = "UNKNOWN"
+                else:
+                    # Here for fun lets say we wanted the dates to be in a different format from
+                    # What the csv records. Currently the data are in yyyy-mm-dd but lets convert to 
+                    # The format: yyyy MON dd 
+                    date_split = transaction_date.split('-')
+                    # Many possible ways to do this but I will just define a quick dictionary to help
+                    num_to_mon = {"01" : "JAN", "02" : "FEB", "03" : "MAR", "04" : "APR", "05" : "MAY", "06" : "JUN",
+                     "07" : "JUL", "08" : "AUG", "09" : "SEP", "10" : "OCT", "11" : "NOV", "12" : "DEC"}
+                    transaction_date = f"{date_split[0]} {num_to_mon[date_split[1]]} {date_split[2]}"
+                    #transaction_date = f"{date_split[1]}/{date_split[2]}/{date_split[0]}"
+
+            else:
+                transaction_date = "UNKNOWN"
+
+
+            # Here we take all the work we did and write the clean line into the output sheet
+            writer.writerow({"Transaction ID" : id, "Item" : item, "Quantity" : quantity, "Price Per Unit" : price_per_unit,
+             "Total Spent" : total_spent, "Payment Method" : payment_method, "Location" : location,
+              "Transaction Date" : transaction_date})
+            
+
+
+
